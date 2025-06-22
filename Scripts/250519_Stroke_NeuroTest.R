92183d2c989a4561162631cf59532f846d4fa30e
JTampe <jftampe@mac.com>
2025-06-22 15:14:25 +0200

    Updated RF model

--- Scripts/250519_Stroke_NeuroTest.R
+++ Scripts/250519_Stroke_NeuroTest.R
@@ -44,6 +44,9 @@ library(mice)
 library(lmerTest)
 library(beepr)
 #library(Seurat)
+library(randomForest)
+library(caret)
+library(tidyr)
 
 thematic_on(bg = "white", fg = "black", accent = "blue")
 
@@ -1124,6 +1127,68 @@ perform_linear_regression_correlation <- function(data, variable, folder_prefix)
     return(correlation_results)
 }
 
+# function for random forrest
+run_rf_analysis <- function(data, features, target, prefix = "RF_Model", plot_folder = "RF_Plots", imp_folder = "RF_Importance_Plots", ntree = 500) {
+    library(randomForest)
+    library(ggplot2)
+    library(dplyr)
+    library(caret)
+    
+    # Create output folders if they don't exist
+    if (!dir.exists(plot_folder)) dir.create(plot_folder)
+    if (!dir.exists(imp_folder)) dir.create(imp_folder)
+    
+    # Prepare data
+    data <- data %>%
+        select(all_of(c(features, target))) %>%
+        drop_na()
+    
+    if (nrow(data) < 10) {
+        warning("Not enough data to train the model.")
+        return(NULL)
+    }
+    
+    # Split data
+    set.seed(123)
+    train_index <- createDataPartition(data[[target]], p = 0.8, list = FALSE)
+    train_data <- data[train_index, ]
+    test_data <- data[-train_index, ]
+    
+    # Train model
+    rf_model <- randomForest(as.formula(paste(target, "~ .")), data = train_data, importance = TRUE, ntree = ntree)
+    
+    # Predict
+    predictions <- predict(rf_model, newdata = test_data)
+    
+    # Save prediction plot
+    plot_data <- data.frame(Actual = test_data[[target]], Predicted = predictions)
+    p <- ggplot(plot_data, aes(x = Actual, y = Predicted)) +
+        geom_point(color = "steelblue", alpha = 0.7) +
+        geom_abline(slope = 1, intercept = 0, linetype = "dashed", color = "darkred") +
+        labs(title = paste("RF Predictions vs Actual -", prefix),
+             x = paste("Actual", target), y = paste("Predicted", target)) +
+        theme_minimal()
+    ggsave(filename = file.path(plot_folder, paste0("RF_Pred_vs_Actual_", prefix, ".png")), plot = p)
+    
+    # Extract importance
+    imp <- importance(rf_model)[, 1]
+    imp_df <- data.frame(Feature = names(imp), Importance = as.vector(imp)) %>%
+        arrange(desc(Importance))
+    
+    # Save importance table
+    write.csv(imp_df, file.path(imp_folder, paste0("RF_Feature_Importances_", prefix, ".csv")), row.names = FALSE)
+    
+    # Save ggplot version of importance
+    p_imp <- ggplot(imp_df, aes(x = reorder(Feature, Importance), y = Importance)) +
+        geom_bar(stat = "identity", fill = "steelblue") +
+        coord_flip() +
+        labs(title = paste("Variable Importance -", prefix), x = "Feature", y = "Mean Decrease in Accuracy") +
+        theme_minimal()
+    ggsave(filename = file.path(imp_folder, paste0("VarImpPlot_", prefix, "_ggplot.png")), plot = p_imp)
+    
+    return(list(rf_model = rf_model, imp_df = imp_df))
+}
+
 ## *Data* ---------------------------------------------------------------------
 
 #### Loading of the Data -----------------------------------------
@@ -1307,7 +1372,7 @@ Metadata_NeuroTest <- as.data.frame(read_excel("Metadata_NeuroTest.xlsx",
                                                              "numeric", "numeric", "numeric", 
                                                              "numeric", "numeric")))
 
-# Create NHISS_End column based on TP4 -------------------------
+# Create NHISS_End column based on TP4 
 Metadata_NeuroTest$NHISS_End <- NA
 
 # Get TP4 NHISS values
@@ -1361,6 +1426,11 @@ nhiss_diff <- Metadata_NeuroTest %>%
 # Join NHISS_Ratio back to Metadata_NeuroTest
 Metadata_NeuroTest <- Metadata_NeuroTest %>%
     left_join(nhiss_diff, by = "SampleID")
+
+Age_Sex <- metadataP %>% select ("SampleID","Sex", "Age")
+
+Metadata_NeuroTest <- merge(Metadata_NeuroTest, Age_Sex, by = "SampleID")
+
 
 dataAll <- merge(dataEXPAND, metadataP, by = "SampleID", all.x = TRUE)
 #### match the right controls
@@ -3941,12 +4011,10 @@ sigGenes <- filter(Age_correlation_capZ, p.value <= 0.05)
 unique(sigGenes$Gene)
 
 ### Random Forrest Analysis over all --------------------------------------------------
-library(randomForest)
-library(caret)
-library(tidyr)
 
 # extracte genes of the data set
 genes_tp2 <- unique(dataFINALmean$Gene)
+dataFINALmean  <- merge(dataFINALmean, Age_Sex, by = "SampleID")
 
 # make a separate data frame for each value:
 Data_meanCapZ <- dataFINALmean  %>% select(all_of(c("SampleID", "Timepoint", "Subpopulation", "Gene", "mean_capZ"))) 
@@ -3964,152 +4032,91 @@ matrix_sdCapZ <-  pivot_wider(Data_sdCapZ , names_from = Gene, values_from = sd_
 matrix_meanCapZ <- merge(matrix_meanCapZ, Metadata_NeuroTest, by = c("SampleID", "Timepoint"), all.x = TRUE)
 matrix_sdCapZ <- merge(matrix_sdCapZ, Metadata_NeuroTest, by = c("SampleID", "Timepoint"), all.x = TRUE)
 
-# Create output folders
-plot_folder <- "RF_Plots"
-imp_folder <- "RF_Importance_Plots"
-if (!dir.exists(plot_folder)) dir.create(plot_folder)
-if (!dir.exists(imp_folder)) dir.create(imp_folder)
+rf_data <- matrix_meanCapZ %>% filter(Timepoint == "TP2")
+
+result <- run_rf_analysis(
+    data = rf_data,
+    features = c("Subpopulation", "Age", "Sex", "NHISS", "mRS", "Barthel", "MoCA", "HADS_Anxiety", "HADS_Depression", 
+                 as.vector(genes_tp2)),
+    target = "NHISS_Diff",
+    prefix = "NHISS_Diff"
+)
+
+# Access the model and importance table
+rf_model <- result$rf_model
+imp_df <- result$imp_df
+
+important_features <- imp_df %>%
+    filter(Importance > 2) %>%
+    pull(Feature)
+
+result_important <- run_rf_analysis(
+    data = matrix_meanCapZ %>% filter(Timepoint == "TP2"),
+    features = important_features,
+    target = "NHISS_Diff",
+    prefix = "NHISS_Diff_important2"
+)
+
+gene_panel <- c("CD33", "CD169", "TNFA", "TGFB1", "CD38")
 
 
-# Filter for TP2
-model_data <- matrix_meanCapZ %>% filter(Timepoint == "TP2")
+result_important <- run_rf_analysis(
+    data = matrix_meanCapZ %>% filter(Timepoint == "TP2"),
+    features = c("Subpopulation", "Age", "Sex", "NHISS", "mRS", "Barthel", "MoCA", "HADS_Anxiety", "HADS_Depression", 
+                 as.vector(gene_panel)),
+    target = "NHISS_Diff",
+    prefix = "NHISS_Diff_panel"
+)
 
-# Define features and target
-features <- c("Subpopulation", "Age", "Sex", "NHISS", "mRS", "Barthel", "MoCA", "HADS_Anxiety", "HADS_Depression",
-              as.vector(genes_tp2))
-#target <- "NHISS_Ratio"
-target <- "NHISS_Diff"
-#target <- "NHISS_End"
+## split by Subpopulation
+# all
+result_important <- run_rf_analysis(
+    data = matrix_meanCapZ %>% filter(Timepoint == "TP2" & Subpopulation == "all"),
+    features = c("Subpopulation", "Age", "Sex", "NHISS", "mRS", "Barthel", "MoCA", "HADS_Anxiety", "HADS_Depression", 
+                 as.vector(genes_tp2)),
+    target = "NHISS_Diff",
+    prefix = "NHISS_Diff_all"
+)
+# all
+result_important <- run_rf_analysis(
+    data = matrix_meanCapZ %>% filter(Timepoint == "TP2" & Subpopulation == "all"),
+    features = c("Subpopulation", "Age", "Sex", "NHISS", "mRS", "Barthel", "MoCA", "HADS_Anxiety", "HADS_Depression", 
+                 as.vector(genes_tp2)),
+    target = "NHISS_Diff",
+    prefix = "NHISS_Diff_all"
+)
+# classical
+result_important <- run_rf_analysis(
+    data = matrix_meanCapZ %>% filter(Timepoint == "TP2" & Subpopulation == "classical"),
+    features = c("Subpopulation", "Age", "Sex", "NHISS", "mRS", "Barthel", "MoCA", "HADS_Anxiety", "HADS_Depression", 
+                 as.vector(genes_tp2)),
+    target = "NHISS_Diff",
+    prefix = "NHISS_Diff_classical"
+)
+# intermediate
+result_important <- run_rf_analysis(
+    data = matrix_meanCapZ %>% filter(Timepoint == "TP2" & Subpopulation == "intermediate"),
+    features = c("Subpopulation", "Age", "Sex", "NHISS", "mRS", "Barthel", "MoCA", "HADS_Anxiety", "HADS_Depression", 
+                 as.vector(genes_tp2)),
+    target = "NHISS_Diff",
+    prefix = "NHISS_Diff_intermediate"
+)
+# nonclassical
+result_important <- run_rf_analysis(
+    data = matrix_meanCapZ %>% filter(Timepoint == "TP2" & Subpopulation == "nonclassical"),
+    features = c("Subpopulation", "Age", "Sex", "NHISS", "mRS", "Barthel", "MoCA", "HADS_Anxiety", "HADS_Depression", 
+                 as.vector(genes_tp2)),
+    target = "NHISS_Diff",
+    prefix = "NHISS_Diff_nonclassical"
+)
 
-# Convert categorical variables to factors
-model_data$Sex <- as.factor(model_data$Sex)
-#model_data$Category <- as.factor(model_data$Category)
-model_data$Subpopulation <- as.factor(model_data$Subpopulation)
-
-
-model_data <- model_data %>%
-    select(all_of(c(features, target))) %>%
-    drop_na()
-
-# Skip if not enough data --> make a warning if there are too little datasets
-# nrow(model_data) < 10
-
-# Split data
-set.seed(123)
-train_index <- createDataPartition(model_data[[target]], p = 0.8, list = FALSE)
-train_data <- model_data[train_index, ]
-test_data <- model_data[-train_index, ]
-
-# Train model
-rf_model <- randomForest(NHISS_Diff ~ ., data = train_data, importance = TRUE, ntree = 500)
-
-
-# Predict
-predictions <- predict(rf_model, newdata = test_data)
-
-# Save prediction plot
-plot_data <- data.frame(Actual = test_data[[target]], Predicted = predictions)
-p <- ggplot(plot_data, aes(x = Actual, y = Predicted)) +
-    geom_point(color = "steelblue", alpha = 0.7) +
-    geom_abline(slope = 1, intercept = 0, linetype = "dashed", color = "darkred") +
-    labs(title = paste("RF Predictions vs Actual - NHISS_Diff"),
-         x = "Actual NHISS_Diff", y = "Predicted NHISS_Diff") +
-    theme_minimal()
-ggsave(filename = file.path(plot_folder, paste0("RF_Pred_vs_Actual_NHISS_Diff.png")), plot = p)
-
-# Save variable importance plot
-png(filename = file.path(imp_folder, paste0("VarImpPlot_NHISS_Diff.png")), width = 800, height = 600)
-varImpPlot(rf_model, main = paste("Variable Importance - NHISS_Diff"))
-dev.off()
-
-# Extract importance
-imp <- importance(rf_model)[, 1]  # MeanDecreaseAccuracy
-imp_df <- data.frame(t(imp))
-
-# Save to CSV
-write.csv(imp_df, "RF_Feature_Importances_Pivot_NHISS_Diff.csv", row.names = FALSE)
-
-imp_df <- data.frame(Feature = names(imp), Importance = as.vector(imp))
-
-imp_df <- imp_df %>% arrange(desc(Importance))
-
-
-imp_df <- data.frame(Feature = rownames(importance(rf_model)), Importance = importance(rf_model)[, 1])
-p_imp <- ggplot(imp_df, aes(x = reorder(Feature, Importance), y = Importance)) +
-    geom_bar(stat = "identity", fill = "steelblue") +
-    coord_flip() +
-    labs(title = "Variable Importance", x = "Feature", y = "Mean Decrease in Accuracy") +
-    theme_minimal()
-ggsave(filename = file.path(imp_folder, "VarImpPlot_NHISS_Diff_ggplot.png"), plot = p_imp)
-
-png(filename = file.path(imp_folder, paste0("VarImpPlot_NHISS_Diff_compared.png")), width = 800, height = 600)
-varImpPlot(rf_model, main = paste("Variable Importance compared - NHISS_Diff"))
-dev.off()
-
-### for NHISS_End instead of Diff -----------------
-target <- "NHISS_End"
-
-model_data <- matrix_meanCapZ %>% filter(Timepoint == "TP2")
-
-model_data <- model_data %>%
-    select(all_of(c(features, target))) %>%
-    drop_na()
-
-# Skip if not enough data --> make a warning if there are too little datasets
-# nrow(model_data) < 10
-
-# Split data
-set.seed(123)
-train_index <- createDataPartition(model_data[[target]], p = 0.8, list = FALSE)
-train_data <- model_data[train_index, ]
-test_data <- model_data[-train_index, ]
-
-# Train model
-rf_model <- randomForest(NHISS_End ~ ., data = train_data, importance = TRUE, ntree = 500)
-
-
-# Predict
-predictions <- predict(rf_model, newdata = test_data)
-
-# Save prediction plot
-plot_data <- data.frame(Actual = test_data[[target]], Predicted = predictions)
-p <- ggplot(plot_data, aes(x = Actual, y = Predicted)) +
-    geom_point(color = "steelblue", alpha = 0.7) +
-    geom_abline(slope = 1, intercept = 0, linetype = "dashed", color = "darkred") +
-    labs(title = paste("RF Predictions vs Actual - NHISS_End"),
-         x = "Actual NHISS_End", y = "Predicted NHISS_End") +
-    theme_minimal()
-ggsave(filename = file.path(plot_folder, paste0("RF_Pred_vs_Actual_NHISS_End.png")), plot = p)
-
-# Save variable importance plot
-png(filename = file.path(imp_folder, paste0("VarImpPlotNHISS_End.png")), width = 800, height = 600)
-varImpPlot(rf_model, main = paste("Variable Importance"))
-dev.off()
-
-# Extract importance
-imp <- importance(rf_model)[, 1]  # MeanDecreaseAccuracy
-imp_df <- data.frame(t(imp))
-
-# Save to CSV
-write.csv(imp_df, "RF_Feature_Importances_Pivot_NHISS_End.csv", row.names = FALSE)
-
-imp_df <- data.frame(Feature = names(imp), Importance = as.vector(imp))
-
-imp_df <- imp_df %>% arrange(desc(Importance))
-
-
-imp_df <- data.frame(Feature = rownames(importance(rf_model)), Importance = importance(rf_model)[, 1])
-p_imp <- ggplot(imp_df, aes(x = reorder(Feature, Importance), y = Importance)) +
-    geom_bar(stat = "identity", fill = "steelblue") +
-    coord_flip() +
-    labs(title = "Variable Importance", x = "Feature", y = "Mean Decrease in Accuracy") +
-    theme_minimal()
-ggsave(filename = file.path(imp_folder, "VarImpPlot_NHISS_End_ggplot.png"), plot = p_imp)
-
-png(filename = file.path(imp_folder, paste0("VarImpPlot_NHISS_End_compared.png")), width = 800, height = 600)
-varImpPlot(rf_model, main = paste("Variable Importance compared - NHISS_End"))
-dev.off()
-
+### for NHISS_End instead of Diff ----------------------------------
+result_End <- run_rf_analysis(
+    data = matrix_meanCapZ %>% filter(Timepoint == "TP2"),
+    features = c("Subpopulation", "Age", "Sex", "NHISS", "mRS", "Barthel", "MoCA", "HADS_Anxiety", "HADS_Depression", as.vector(genes_tp2)),
+    target = "NHISS_End",
+    prefix = "NHISS_End"
+)
 
 # *Sig Regression - Gene Overview* ----------------------------------------------------
 # with a mean for all Timepoints
