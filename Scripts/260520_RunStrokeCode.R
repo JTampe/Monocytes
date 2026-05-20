5d94b610853d3d957719c1da86a74a74e96d4a40
JTampe <jftampe@mac.com>
2026-05-20 13:01:07 +0200

    more

---
+++ Scripts/260520_RunStrokeCode.R
@@ -0,0 +1,14 @@
+source("~/Github/Monocytes/Scripts/260518_Stroke01_Set Up and Functions.R")
+
+source("~/Github/Monocytes/Scripts/260518_Stroke02_Read and combine data.R")
+
+source("~/Github/Monocytes/Scripts/260518_Stroke03_DotplotPCANormalDistribution.R")
+
+source("~/Github/Monocytes/Scripts/260518_Stroke04_FACSstatistics.R")
+
+source("~/Github/Monocytes/Scripts/260518_Stroke05_NeurologicalTestRegressions.R")
+# Housekeeping genes?
+
+source("~/Github/Monocytes/Scripts/260518_Stroke06_DotplotsHeatmaps.R")
+
+source("~/Github/Monocytes/Scripts/260518_Stroke07_VulcanoPlots.R")
